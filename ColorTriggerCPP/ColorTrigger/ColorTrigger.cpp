9d64b3d6ab723dce53e6c7ffd519e8d51485c184
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2023-09-19 09:08:38 -0400

    Update ColorTrigger.cpp
    Update
    --------
    - Изменён метод GetPixelColor, в более простой. 
    - Добавлено получение высокого приоритета, нужно для точной работы во всех играх или программах, так же для мгновенного отклика.
    - Исправлено изменение порога, теперь он изменяется нормально стрелкой верх и вниз
    - Исрпавлены бинды Mouse3 - Mouse4 на Mouse4 - Mouse5 (Боковые кнопки мыши)
    - Добавлен более точный лог изменение цвета пикселя в формате RGB
    - Добавлено скрытие консоли, он скрывается в фоновые процессы и так же можно вернуть в запущенные приложения, в основе полезно для видео, демонстраций экрана или просто что бы не мусолило глаза (Скрыть/вернуть на Insert - Delete)

--- ColorTriggerCPP/ColorTrigger/ColorTrigger.cpp
+++ ColorTriggerCPP/ColorTrigger/ColorTrigger.cpp
@@ -1,55 +1,54 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
-#include <gdiplus.h>
 #include <string>
 #include <thread>
+#include <iomanip>
 
 using namespace std;
-using namespace Gdiplus;
 
 bool IsKeyPressed(int vKey) {
     return GetAsyncKeyState(vKey) < 0;
 }
 
-Color GetPixelColor(int x, int y) {
+COLORREF GetPixelColor(int x, int y) {
     HDC hScreenDC = GetDC(NULL);
     COLORREF colorRef = GetPixel(hScreenDC, x, y);
     ReleaseDC(NULL, hScreenDC);
-    return Color::MakeARGB(255, GetRValue(colorRef), GetGValue(colorRef), GetBValue(colorRef));
+    return colorRef;
 }
 
 void MouseClick() {
     INPUT input = { 0 };
-    //this_thread::sleep_for(chrono::milliseconds(10));
+    //this_thread::sleep_for(chrono::milliseconds(10)); //Для того, что бы не было мгновенного клика.
     input.type = INPUT_MOUSE;
     input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
     SendInput(1, &input, sizeof(INPUT));
     this_thread::sleep_for(chrono::milliseconds(1));
-    ZeroMemory(&input, sizeof(INPUT));
-    input.type = INPUT_MOUSE;
     input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
     SendInput(1, &input, sizeof(INPUT));
 }
 
 int main() {
+    if (!SetPriorityClass(GetCurrentProcess(), HIGH_PRIORITY_CLASS)) {
+        cerr << "Ошибка установки высокого приоритета процесса, запустите от имени админа!" << endl;
+        return 1;
+    }
     setlocale(LC_ALL, "");
     SetConsoleTitle(L"https://t.me/devilLucifer69 / DS: desirepro");
 
     int threshold;
     cout << "Введите значение порога (от 4 до 20): ";
-    while (true) {
+    cin >> threshold;
+    while (threshold < 4 || threshold > 20) {
+        cout << "Некорректное значение порога. Пожалуйста, введите число от 4 до 20." << endl;
         cin >> threshold;
-        if (threshold >= 4 && threshold <= 20)
-            break;
-        else
-            cout << "Некорректное значение порога. Пожалуйста, введите число от 4 до 20." << endl;
     }
 
-    cout << "Выберите бинд (X, F, LALT, Mouse3, Mouse4): ";
+    cout << "Выберите бинд (X, F, LALT, Mouse4, Mouse5): ";
     string selectedBind;
     cin >> selectedBind;
 
-    cout << "Good!" << endl;
+    cout << "Good! Чтобы скрыть консоль, нажмите на Delete/Insert" << endl;
     int bindKey = 0;
     if (selectedBind == "X") {
         bindKey = 'X';
@@ -60,41 +59,51 @@ int main() {
     else if (selectedBind == "LALT") {
         bindKey = VK_LMENU;
     }
-    else if (selectedBind == "Mouse3") {
-        bindKey = VK_MBUTTON;
-    }
     else if (selectedBind == "Mouse4") {
         bindKey = VK_XBUTTON1;
     }
-    while (true) {
-        if (IsKeyPressed(VK_UP)) {
-            if (threshold < 20)
-                threshold++;
-            cout << "Порог увеличен до " << threshold << endl;
-        }
-        else if (IsKeyPressed(VK_DOWN)) {
-            if (threshold > 4)
-                threshold--;
-            cout << "Порог уменьшен до " << threshold << endl;
-        }
+    else if (selectedBind == "Mouse5") {
+        bindKey = VK_XBUTTON2;
+    }
 
+    while (true) {
+        if (IsKeyPressed(VK_UP) && threshold < 20) {
+            cout << "Порог увеличен до " << ++threshold << endl;
+            Sleep(200);
+        }
+        else if (IsKeyPressed(VK_DOWN) && threshold > 4) {
+            cout << "Порог уменьшен до " << --threshold << endl;
+            Sleep(200);
+        }
+        if (IsKeyPressed(VK_INSERT) || IsKeyPressed(VK_DELETE)) {
+            HWND hwnd = GetConsoleWindow();
+            ShowWindow(hwnd, IsWindowVisible(hwnd) ? SW_HIDE : SW_SHOW);
+            Sleep(200);
+        }
         if (IsKeyPressed(bindKey)) {
             POINT p;
             GetCursorPos(&p);
             int x = p.x + 2;
             int y = p.y + 2;
-            Color color1 = GetPixelColor(x, y);
+            COLORREF color1 = GetPixelColor(x, y);
 
             GetCursorPos(&p);
             x = p.x + 2;
             y = p.y + 2;
-            Color color2 = GetPixelColor(x, y);
+            COLORREF color2 = GetPixelColor(x, y);
 
-            if (abs(color1.GetR() - color2.GetR()) > threshold ||
-                abs(color1.GetG() - color2.GetG()) > threshold ||
-                abs(color1.GetB() - color2.GetB()) > threshold) {
-                cout << "Color: " << color2.GetValue() << endl;
-                MouseClick(); 
+            BYTE r1 = GetRValue(color1);
+            BYTE g1 = GetGValue(color1);
+            BYTE b1 = GetBValue(color1);
+
+            BYTE r2 = GetRValue(color2);
+            BYTE g2 = GetGValue(color2);
+            BYTE b2 = GetBValue(color2);
+
+            if (abs(r1 - r2) > threshold || abs(g1 - g2) > threshold || abs(b1 - b2) > threshold) {
+                cout << "с RGB:(" << static_cast<int>(r1) << ", " << static_cast<int>(g1) << ", " << static_cast<int>(b1) << ") на ";
+                cout << "RGB(" << static_cast<int>(r2) << ", " << static_cast<int>(g2) << ", " << static_cast<int>(b2) << "). Произведён клик!" << endl;
+                MouseClick();
             }
         }
     }
